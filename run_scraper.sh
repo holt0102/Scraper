305d3b00ba94d86f0e33d80bbce98659b75fd627
Francisco Valente <fco.valente.castro@gmail.com>
2020-01-02 22:32:37 -0600

    Update run_scraper.sh

--- run_scraper.sh
+++ run_scraper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# A script to quickly execute the default scraping jobs.
+# A bash script to quickly execute the default scraping jobs.
 
 cd 'amazon_scraper'
 scrapy crawl amazon_reviews -a filename=urls.txt
