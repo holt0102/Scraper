cf7ea4a5626f01f2b5de6133d1cbf13a6a345cf0
IsaacRodgz <chack.rb@gmail.com>
2020-01-02 19:02:54 -0600

    [ADD] Follow link to next page

--- amazon_scraper/amazon_scraper/spiders/amazon_reviews_spider.py
+++ amazon_scraper/amazon_scraper/spiders/amazon_reviews_spider.py
@@ -14,25 +14,33 @@ class AmazonReviewsSpider(scrapy.Spider):
     allowed_domains = ['amazon.com.mx']
 
     # Base URL with search "iphone x smartphone"
-    start_urls = ['https://www.amazon.com.mx/s?k=iphone+x+smartphone&rh=n%3A9687460011&__mk_es_MX=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss']
+    start_urls = ['https://www.amazon.com.mx/s?'
+                  'k=iphone+x+smartphone&rh=n%3'
+                  'A9687460011&__mk_es_MX=%C3%8'
+                  '5M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss']
 
     # Spider configuration
-    custom_settings = {'CONCURRENT_REQUESTS': '1', 'DEFAULT_ITEM_CLASS' : 'AmazonItem', 'ROBOTSTXT_OBEY': 'False'}
+    custom_settings = {'CONCURRENT_REQUESTS': '1',
+                       'DEFAULT_ITEM_CLASS' : 'AmazonItem',
+                       'ROBOTSTXT_OBEY': 'False'}
 
     # Defining a Scrapy parser
     def parse(self, response):
 
         # Parse each result (24 per page)
 
-        for item in response.xpath('//span[@cel_widget_id="SEARCH_RESULTS-SEARCH_RESULTS"]'):
+        for item in response.xpath('//span[@cel_widget_id='
+                                   '"SEARCH_RESULTS-SEARCH_RESULTS"]'):
 
             product = AmazonItem()
 
             item_bs4 = BeautifulSoup(item.get(), "lxml")
 
-            price_bs4 = item_bs4.find("span", {"class" : "a-price-whole"})
-            name_bs4 = item_bs4.find("span", {"class" : "a-size-base-plus a-color-base a-text-normal"})
-            rating_bs4 = item_bs4.find("span", {"class" : "a-icon-alt"})
+            price_bs4 = item_bs4.find("span", {"class": "a-price-whole"})
+            name_bs4 = item_bs4.find("span", {"class": "a-size-base-plus "
+                                                       "a-color-base "
+                                                       "a-text-normal"})
+            rating_bs4 = item_bs4.find("span", {"class": "a-icon-alt"})
 
             # Product sale price
 
