210776f0267ebc8d5e59f51c678964281734b20b
crispu <jose.alvarado@cimat.mx>
2020-01-04 04:32:49 -0600

    django-app
    Integración con una base de datos existente (MySQL)
    Aún no hay visualización de datos

---
+++ app/scraper/scraper/Apps/GestionScraper/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from scraper.Apps.GestionScraper.models import *
+
+# Register your models here.
+
+admin.site.register(Article)
---
+++ app/scraper/scraper/Apps/GestionScraper/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GestionscraperConfig(AppConfig):
+    name = 'GestionScraper'
